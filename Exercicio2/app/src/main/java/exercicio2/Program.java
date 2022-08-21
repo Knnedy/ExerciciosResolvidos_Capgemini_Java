642bf19922f8a379fc75bb2e71ef09a5c751f82c
kennedy <dkennyvs@gmail.com>
2022-08-21 10:07:50 -0300

    Soma, subtração, multiplicação e divisão

---
+++ Exercicio2/app/src/main/java/exercicio2/Program.java
@@ -0,0 +1,34 @@
+package exercicio2;
+
+import java.util.Scanner;
+
+/**
+ * Faça um algoritmo que receba dois números e ao final mostre a soma,
+ * subtração, multiplicação e a divisão dos dois números lidos 
+ */
+
+public class Program {
+  
+  public static void main(String[] args) {
+  
+    try(Scanner sc = new Scanner(System.in)) {
+
+
+      System.out.print("X: ");
+      int x = sc.nextInt();
+      System.out.println("X: ");
+      int y = sc.nextInt();
+
+      int soma = x + y;
+      int subtracao = x - y;
+      int multiplicacao = x * y;
+      int divisao = x / y;
+
+      System.out.println("Soma: " + soma);
+      System.out.println("Subtração: " + subtracao);
+      System.out.println("Multiplicação: " + multiplicacao);
+      System.out.println("Divisão: " + divisao);
+
+    }
+  }
+}
