4d1624dccc8b0ab18289408dfb37e5e797f827c3
kennedy <dkennyvs@gmail.com>
2022-08-21 16:02:47 -0300

    Conversor dollar/real

---
+++ Exercicio8/app/src/main/java/exercicio8/Program.java
@@ -0,0 +1,37 @@
+package exercicio8;
+
+/*
+ * Elabore um algoritmo que efetue a apresentação do valor da conversão em real
+ * (R$) de um valor lido em dólar (US$). O algoritmo deverá solicitar o valor
+ * da cotação do dólar e também aquantidade de dólares disponíveis com o
+ * usuário
+ */
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		try(Scanner sc = new Scanner(System.in)) {
+
+			Locale.setDefault(Locale.US);
+
+			double cotacaoDollar; // Cotação atual
+			double meuDollar;
+			double real;
+
+			System.out.print("Cotação atual do dollar: US$");
+			cotacaoDollar = sc.nextDouble();
+
+			System.out.print("Quantos doláres você tem? ");
+			meuDollar = sc.nextDouble();
+
+			real = meuDollar * cotacaoDollar;
+
+			System.out.println("Convertendo US$" + String.format("%.2f", meuDollar) + 
+												 " para R$" + String.format("%.2f", real));
+												 
+		}
+	}
+}
