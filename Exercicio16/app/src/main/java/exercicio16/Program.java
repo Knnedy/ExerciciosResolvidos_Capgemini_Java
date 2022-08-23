55984c644fce78cb01eb2e3c8c0755ddd977a322
kennedy <dkennyvs@gmail.com>
2022-08-23 08:30:35 -0300

    Média de três notas

--- Exercicio16/app/src/main/java/exercicio16/Program.java
+++ Exercicio16/app/src/main/java/exercicio16/Program.java
@@ -17,11 +17,15 @@ public class Program {
     try(Scanner sc = new Scanner(System.in)) {
 
       Locale.setDefault(Locale.US);
-
+      
+      String nome;
       double nota1;
       double nota2;
       double nota3;
       double media;
+
+      System.out.println("Nome do aluno: ");
+      nome = sc.nextLine();
 
       System.out.println("Entre com duas três notas: ");
       nota1 = sc.nextDouble();
@@ -31,16 +35,19 @@ public class Program {
       media = (nota1 + nota2 + nota3) / 3;
 
       if(media>= 7) {
-        System.out.println("Sua média: " + String.format("%.1f", media) +
-        " | VOCÊ FOI APROVADO!" );
+        System.out.println("Nome do aluno: " + nome + 
+        "\nSua média: " + String.format("%.1f", media) +
+        "\nVOCÊ FOI APROVADO!" );
       }
       else if (media >= 5.0 && media <= 6.9) {
-        System.out.println("Sua média: " + String.format("%.1f", media) +
-        " | VOCÊ ESTÁ EM RECUPERAÇÃO!" );
+        System.out.println("Nome do aluno: " + nome + 
+        "\nSua média: " + String.format("%.1f", media) +
+        "\nVOCÊ ESTÁ EM RECUPERAÇÃO!");
       } 
       else {
-        System.out.println("Sua média: " + String.format("%.1f", media) +
-        " | VOCÊ FOI REPROVADO!");
+        System.out.println("Nome do aluno: " + nome + 
+        "\nSua média: " + String.format("%.1f", media) +
+        "\nVOCÊ FOI REPROVADO!");
       }
 
     }
